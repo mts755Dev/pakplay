2fee8b6165de7a535a17f8e4a68d6906a674575f
Muhammad Talha Saleem <muhammadtalhasaleem.755@gmail.com>
2026-01-13 20:17:51 +0500

    Add dynamic rendering to Home component to prevent prerendering at build time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { IndexPage } from "@/components/pages/IndexPage";
 import { fetchFeaturedVenues, fetchAppStats, fetchTopSports } from "@/lib/server-actions";
 
+// Force dynamic rendering - don't prerender at build time
+export const dynamic = 'force-dynamic';
+
 // Server component with SSR - Fetch all data in parallel
 export default async function Home() {
   const [featuredVenues, appStats, topSports] = await Promise.all([
